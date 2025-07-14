42b0fb97667af44776633d283b85ddb367cf9c38
tamim-111 <tamim144362@gmail.com>
2025-07-15 02:21:28 +0600

    feat: add sellerSidebar component

--- src/components/Dashboard/Sidebar/SellerSidebar/SellerSidebar.jsx
+++ src/components/Dashboard/Sidebar/SellerSidebar/SellerSidebar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import SidebarLink from '../SidebarLink';
+import { FaPills, FaHistory, FaBullhorn } from 'react-icons/fa'
 
 const SellerSidebar = () => {
     return (
-        <div>
-            SellerSidebar
+        <div className='space-y-2 mt-4'>
+            <SidebarLink to='/dashboard/seller/manage-medicines' icon={<FaPills />} label='Manage Medicines' />
+            <SidebarLink to='/dashboard/seller/purchase-history' icon={<FaHistory />} label='Purchase History' />
+            <SidebarLink to='/dashboard/seller/ask-for-advertisement' icon={<FaBullhorn />} label='Advertise Request' />
         </div>
     );
 };
