02a08948154449f34152a29c412038915a61d5f7
tamim-111 <tamim144362@gmail.com>
2025-07-14 15:50:53 +0600

    feat: add MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-
+import { Outlet } from 'react-router'
+import Navbar from '../components/Home/Navbar/Navbar'
+import Footer from '../components/Home/Footer/Footer'
 const MainLayout = () => {
     return (
         <div>
-            Main Layout
+            <Navbar />
+            <div className='pt-24 min-h-[calc(100vh-68px)]'>
+                <Outlet />
+            </div>
+            <Footer />
         </div>
-    );
-};
+    )
+}
 
-export default MainLayout;
+export default MainLayout
