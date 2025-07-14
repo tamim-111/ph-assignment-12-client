fa0eeb4ae08c9eb6954452444a54cf55f7970f4c
tamim-111 <tamim144362@gmail.com>
2025-07-15 02:16:02 +0600

    feat: add adminSidebar component

--- src/components/Dashboard/Sidebar/AdminSidebar/AdminSidebar.jsx
+++ src/components/Dashboard/Sidebar/AdminSidebar/AdminSidebar.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { NavLink } from 'react-router';
+import React from 'react'
+import { FaUsers, FaThList, FaMoneyCheckAlt, FaChartBar } from 'react-icons/fa'
+import { RiAdvertisementLine } from 'react-icons/ri'
+import SidebarLink from '../SidebarLink'
 
 const AdminSidebar = () => {
     return (
-        <div>
-            <NavLink>Manage Users</NavLink>
+        <div className='space-y-2 mt-4'>
+            <SidebarLink to='/dashboard/admin/manage-users' icon={<FaUsers />} label='Manage Users' />
+            <SidebarLink to='/dashboard/admin/manage-category' icon={<FaThList />} label='Manage Categories' />
+            <SidebarLink to='/dashboard/admin/payment-management' icon={<FaMoneyCheckAlt />} label='Payment Management' />
+            <SidebarLink to='/dashboard/admin/sales-report' icon={<FaChartBar />} label='Sales Report' />
+            <SidebarLink to='/dashboard/admin/manage-banner-advertise' icon={<RiAdvertisementLine />} label='Banner Advertise' />
         </div>
-    );
-};
+    )
+}
 
-export default AdminSidebar;
+export default AdminSidebar
