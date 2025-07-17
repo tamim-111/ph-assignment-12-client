7ba68c456fb9fa26aff4d9cf804777d9534aa1a7
tamim-111 <tamim144362@gmail.com>
2025-07-18 01:07:58 +0600

    feat(useRole): get user role using useRole hook

---
+++ src/hooks/useRole.js
@@ -0,0 +1,22 @@
+// hooks/useRole.js
+import { useQuery } from '@tanstack/react-query'
+import useAuth from './useAuth'
+import useAxiosSecure from './useAxiosSecure'
+
+const useRole = () => {
+    const axiosSecure = useAxiosSecure()
+    const { user, loading } = useAuth()
+
+    const { data: role = null, isLoading } = useQuery({
+        queryKey: ['user-role', user?.email],
+        enabled: !loading && !!user?.email,
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users?email=${user.email}`)
+            return res.data?.role
+        }
+    })
+    console.log(role)
+    return { role, isLoading }
+}
+
+export default useRole
