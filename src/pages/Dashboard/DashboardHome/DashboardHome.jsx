882f90747b4c2aa601f7017ef25964850d8fe12e
tamim-111 <tamim144362@gmail.com>
2025-07-18 01:16:45 +0600

    feat: add dashboard home page

--- src/pages/Dashboard/DashboardHome/DashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import useRole from '../../../hooks/useRole';
+import CustomerDashboardHome from '../Customer/CustomerDashboardHome/CustomerDashboardHome';
+import LoadingSpinner from '../../../components/Spinner/LoadingSpinner';
+import AdminDashboardHome from '../Admin/AdminDashboardHome/AdminDashboardHome';
+import SellerDashboardHome from '../Seller/SellerDashboardHome/SellerDashboardHome';
 
 const DashboardHome = () => {
+    const { role, isLoading } = useRole()
+
+    if (isLoading) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
+
+    if (role === 'customer') {
+        return <CustomerDashboardHome />
+    }
+
+    if (role === 'seller') {
+        return <SellerDashboardHome />
+    }
+
+    if (role === 'admin') {
+        return <AdminDashboardHome />
+    }
     return (
-        <div>
-            DashboardHome
+        <div className="min-h-screen flex items-center justify-center text-red-500 font-semibold">
+            Unauthorized or unknown role.
         </div>
     );
 };
