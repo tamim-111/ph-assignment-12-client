3bf32e108e865ccdd9d00e18983f8b3ae9071d16
tamim-111 <tamim144362@gmail.com>
2025-07-18 04:54:30 +0600

    style(categoryCard): impove card image style

--- src/components/Home/Category/CategoryCard.jsx
+++ src/components/Home/Category/CategoryCard.jsx
@@ -7,7 +7,7 @@ const CategoryCard = ({ id, name, image, count }) => {
             to={`/categoryDetails/${id}`}
             className='bg-white rounded-xl shadow-md hover:shadow-lg transition-transform hover:scale-[1.02] overflow-hidden border border-[#e0f7ff]'
         >
-            <img src={image} alt={name} className='w-full h-40 object-fill' />
+            <img src={image} alt={name} className='size-40 mx-auto py-5' />
             <div className='p-4 space-y-1'>
                 <h3 className='text-xl font-bold text-[#25A8D6]'>{name}</h3>
                 <p className='text-sm text-gray-600'>{count} medicines</p>
